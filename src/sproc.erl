0062ba30aa4a8d3c1509d5f955c8d8081c6be557
Ivan Lisenkov <lisenkov@cplire.ru>
2013-01-05 01:00:11 +0400

    Fixed interface functions

--- src/sproc.erl
+++ src/sproc.erl
@@ -2,16 +2,19 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% sproc API %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--export([reg/2, where/1]).
+-export([reg/2, where/1, unreg/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 reg(Name, Pid) ->
 	gen_server:call(sproc_srv, {reg, Name, Pid}).
 
+unreg(Name) ->
+	gen_server:cast({unreg, Name}).
+
 where(Name) ->
 	case ets:match(sproc, {Name, '$1'}) of
-		[Pid|_] -> Pid;
+		[[Pid]|_] -> Pid;
 		[] -> not_registered
 	end.
 
